fc420553b0c258af9961de4369486779849a1cb2
MARA MAMEROW <maramamerow@northwesternmutual.com>
2020-07-01 07:55:11 -0500

    Completed chapter 5.2

--- steps/Catalog/then.js
+++ steps/Catalog/then.js
@@ -2,6 +2,11 @@ import { Then } from "cucumber";
 import checkNoResultsError from "../../support/assertions/checkNoResultsError";
 import checkProducts from "../../support/assertions/checkProducts";
 import checkProductsContain from "../../support/assertions/checkProductsContain";
+import checkTitle from "../../support/assertions/checkTitle";
+
+Then("a no results error message is shown", () => {
+  checkNoResultsError();
+});
 
 Then(/^(no )?products are listed$/, notListed => {
   if (notListed) {
@@ -11,10 +16,10 @@ Then(/^(no )?products are listed$/, notListed => {
   }
 });
 
-Then("a no results error message is shown", () => {
-  checkNoResultsError();
-});
-
 Then(/^search results show products related to "(.*)"$/, keyword => {
   checkProductsContain(keyword);
 });
+
+Then(/^the title of the page should be "(.*)"$/, title => {
+  checkTitle(title);
+}); 
