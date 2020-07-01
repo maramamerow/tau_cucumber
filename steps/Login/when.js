b03c5313f3b5293ce7f96425436a5747e1ce1747
MARA MAMEROW <maramamerow@northwesternmutual.com>
2020-07-01 15:52:49 -0500

    Completed Chapter 5.3 Challenge

--- steps/Login/when.js
+++ steps/Login/when.js
@@ -1,7 +1,8 @@
 import { When } from "cucumber";
 import signIn from "../../support/actions/signIn";
 
-When(/^the user tries to use valid credentials, "(.*)" to login$/,  email => {
-    signIn(email);
+When(/^the user tries to use "(valid|invalid)" credentials, "(.*)" to login$/,  
+    (valid,email) => {
+    signIn(valid === "valid" ? true : false, email);
   }
 );
